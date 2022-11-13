5712991aa1d3a01faa852017cf1699eaca3a0a6a
TrixOMG <nikitin.pavel.163@gmail.com>
2022-11-13 12:24:14 +0300

    added table + more styling

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let 
+let table = document.getElementById("expense-table");
+
+//let row = table.insertRow(0);
+
+//let cell1 = table.insertCell(0);
+//let cell2 = table.insertCell(1);
+
+//cell1.innerHTML = 'new cell1';
+//cell2.innerHTML = 'new cell2';
